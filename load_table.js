32da7c42e66883eea9403c35f03334907feb194c
musicedit <139318717+musicedit@users.noreply.github.com>
2023-08-29 12:31:07 +0800

    Update load_table.js

--- load_table.js
+++ load_table.js
@@ -144,7 +144,9 @@ function generateRemixTable(tableId, filenames, page) {
       // if (tableId != 'genre'){
       //   let command = 'remix this music';
       // }
-      cell.innerHTML = command.replaceAll('_', ' ');
+      cell.innerHTML = command.replaceAll('_', ' ').replaceAll('r and b', 'r&b');
+
+      // cell.innerHTML = 'remix this music'
       cell.style.textAlign = "center";
 
       cell = row.insertCell(1);
@@ -209,6 +211,95 @@ function generateRemixTable(tableId, filenames, page) {
     }
   }
 }
+
+
+
+function generateRemixGuideTable(tableId, filenames, page) {
+  let numPerPage = 4;
+  let table = document.getElementById(tableId + '-operation');
+  let nrRows = table.rows.length;
+  for (let i = 1; i < nrRows; i++) {
+    table.deleteRow(1);
+  }
+  const prefix = 'music/remix_' + tableId + '/';
+  const end_idx = page * numPerPage;
+  for (let i = (page - 1) * numPerPage; i < end_idx; i++) {
+    let row = table.insertRow(i % numPerPage + 1);
+    row.style.height = '80px';
+    if (i < filenames.length) {
+      let cell = row.insertCell(0);
+      let reg = /[0-9]+/g;
+      let command = filenames[i].replace(reg,"");
+      // if (tableId != 'genre'){
+      //   let command = 'remix this music';
+      // }
+      // cell.innerHTML = command.replaceAll('_', ' ');
+      cell.innerHTML = 'remix this music'
+      cell.style.textAlign = "center";
+
+      cell = row.insertCell(1);
+      cell.innerHTML = createAudioHTML(prefix + filenames[i] + '_source_acc.wav', false);
+      cell.style.textAlign = "center";
+
+      cell = row.insertCell(2);
+      cell.innerHTML = createAudioHTML(prefix + filenames[i] + '_source_melody.wav', false);
+      cell.style.textAlign = "center";
+
+      cell = row.insertCell(3);
+      cell.innerHTML = createAudioHTML(prefix + filenames[i] + '_target_acc.wav', false);
+      cell.style.textAlign = "center";
+
+      cell = row.insertCell(4);
+      cell.innerHTML = createAudioHTML(prefix + filenames[i] + '_target_melody.wav', false);
+      cell.style.textAlign = "center";
+
+      cell = row.insertCell(5);
+      cell.innerHTML = createAudioHTML(prefix + filenames[i] + '_instructme_acc.wav', false);
+      cell.style.textAlign = "center";
+
+      cell = row.insertCell(6);
+      cell.innerHTML = createAudioHTML(prefix + filenames[i] + '_instructme_melody.wav', false);
+      cell.style.textAlign = "center";
+
+      cell = row.insertCell(7);
+      cell.innerHTML = createAudioHTML(prefix + filenames[i] + '_audit_acc.wav', false);
+      cell.style.textAlign = "center";
+
+      cell = row.insertCell(8);
+      cell.innerHTML = createAudioHTML(prefix + filenames[i] + '_audit_melody.wav', false);
+      cell.style.textAlign = "center";
+
+    } else {
+      let cell = row.insertCell(0);
+      cell.innerHTML = '<br>';
+      cell = row.insertCell(1);
+      cell.innerHTML = '<br>';
+      cell.style.textAlign = "center";
+      cell = row.insertCell(2);
+      cell.innerHTML = '<br>';
+      cell.style.textAlign = "center";
+      cell = row.insertCell(3);
+      cell.innerHTML = '<br>';
+      cell.style.textAlign = "center";
+      cell = row.insertCell(4);
+      cell.innerHTML = '<br>';
+      cell.style.textAlign = "center";
+      cell = row.insertCell(5);
+      cell.innerHTML = '<br>';
+      cell.style.textAlign = "center";
+      cell = row.insertCell(6);
+      cell.innerHTML = '<br>';
+      cell.style.textAlign = "center";
+      cell = row.insertCell(7);
+      cell.innerHTML = '<br>';
+      cell.style.textAlign = "center";
+      cell = row.insertCell(8);
+      cell.innerHTML = '<br>';
+      cell.style.textAlign = "center";
+    }
+  }
+}
+
 
 
 function generateRealTable(tableId, filenames, songnames, page) {
@@ -544,8 +635,8 @@ generateEditTable('replace', replace  , 1);
 
 generateRemixTable('instr', instr  , 1);
 generateRemixTable('genre', genre  , 1);
-generateRemixTable('soft', soft  , 1);
-generateRemixTable('happy', happy  , 1);
+generateRemixGuideTable('soft', soft  , 1);
+generateRemixGuideTable('happy', happy  , 1);
 generateDiversityTable('dedit', dedit  , 1);
 generateDiversityRemixTable('dremix', dremix  , 1);
 generateDiversityTable('sedit', sedit  , 1);
@@ -642,7 +733,7 @@ $(document).ready(function() {
   for (let i = 1; i <= 1; i++) {
     let id = '#soft-operation-' + i;
     $(id).click(function() {
-      generateRemixTable(
+      generateRemixGuideTable(
           'soft',
           soft, i);
       $(id).parent().siblings().removeClass('active');
@@ -656,7 +747,7 @@ $(document).ready(function() {
   for (let i = 1; i <= 1; i++) {
     let id = '#happy-operation-' + i;
     $(id).click(function() {
-      generateRemixTable(
+      generateRemixGuideTable(
           'happy',
           happy, i);
       $(id).parent().siblings().removeClass('active');
